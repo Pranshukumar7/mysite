bbc0c0865654c4dd1cef8493c0a7f03acce37ea7
Pranshu Kumar <pranshukumar8544@gmail.com>
2023-03-07 22:47:51 +0530

    general owner function added

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /polls/
     path('', views.index, name='index'),
+    path('owner', views.owner, name='owner'),
+    # /polls/5/
+    path("<int:question_id>/", views.detail, name='detail'),
+    #/polls/5/result/
+    path("<int:question_id>/result/",views.result,name = 'result'),
+    # /polls/5/vote/
+    path("<int:question_id>/vote/",views.vote,name = 'vote'),
+
+
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,20 @@
 from django.http import HttpResponse
+from .models import Question
 
+def owner(request):
+    return HttpResponse("Hello, world. e6d13316 is the polls index.")
 
 def index(request):
-    return HttpResponse("Hello, world. 24974eb2 is the polls index.")
+    qu = Question.objects.order_by('-pub_date')
+    output = ",".join([q.question_text for q in qu])
+    return HttpResponse(output)
+
+# result
+def result(request,question_id):
+    return HttpResponse("hey you are looking at result of %s" % question_id)
+# detail
+def detail(request,question_id):
+    return HttpResponse("hey you are looking at detail of %s" % question_id)
+#vote
+def vote(request,question_id):
+    return HttpResponse("hey you are voteing of %s" % question_id)
