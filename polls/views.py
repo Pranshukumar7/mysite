a3d9e1119fa87463b5701661915b58643485340d
Pranshu Kumar <pranshukumar8544@gmail.com>
2023-03-08 08:46:17 +0530

    week3 final commit

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse , Http404
 from .models import Question
+
 from django.shortcuts import render
 def owner(request):
     return HttpResponse("Hello, world. e6d13316 is the polls index.")
@@ -14,7 +15,10 @@ def result(request,question_id):
     return HttpResponse("hey you are looking at result of %s" % question_id)
 # detail
 def detail(request,question_id):
-    return HttpResponse("hey you are looking at detail of %s" % question_id)
-#vote
+    try:
+        question = Question.objects.get(pk = question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist.")
+    return render(request,'details.html',{'question':question})
 def vote(request,question_id):
     return HttpResponse("hey you are voteing of %s" % question_id)
